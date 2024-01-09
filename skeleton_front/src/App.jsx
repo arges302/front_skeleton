9bc2688b0dfa427470325dcaca7e75cdb2b1e91f
Seob <mp20002@naver.com>
2024-01-09 17:47:16 +0900

    a

--- skeleton_front/src/App.jsx
+++ skeleton_front/src/App.jsx
@@ -2,17 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 
 import Header from './home/component/Header'
 import Footer from './home/component/Footer'
-import Home from './home/component/Home'
 import HomeMain from './home/HomeMain'
+import UserMain from './user/UserMain'
 
 const App = () => {
-  return(
+  return (
     <div>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<HomeMain/>}/>
+        <Route path='/' element={<HomeMain />} />
+        <Route path='/user/*' element={<UserMain />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- skeleton_server/user/userRouter.js
+++ skeleton_server/user/userRouter.js
@@ -5,7 +5,7 @@ const userDAO = require('./userDAO')
 // 유저업무와 관련된 요청이 들어왔을때 그요청을 처리하는 역할
 router.get('/signup', async (req, res, next) => {
   console.log('user router, signup.......')
-  userDAO.signup({name:'박종섭', email:'seob@seob.com2', password: '1234'}, (resp) => {
+  userDAO.signup({name:'박종섭', email:'seob@seob.com4', password: '1234'}, (resp) => {
     res.send(resp)
   })
 })
